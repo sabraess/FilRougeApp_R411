ae82fe3badac85dd7bff2d03b4260545b5ec6179
TORRI Clara <clara.torri@etu.unice.fr>
2024-03-20 23:01:07 +0100

    commencement filtre

---
+++ FilRouge/app/src/main/java/filrouge/app/FilterActivity.java
@@ -0,0 +1,20 @@
+package filrouge.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+/*
+* Author:
+* Modifier par:
+* permet de filtrer les annonces
+ */
+
+public class FilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+
+    }
+}
