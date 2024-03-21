cb35c902c44915ecedf46536e5dae3e50ca24bbb
Essalah Sabra <sabra.essalah@etu.unice.fr>
2024-03-21 09:23:21 +0100

    Configuration du firebase

--- FilRouge/app/build.gradle.kts
+++ FilRouge/app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    alias(libs.plugins.androidApplication)
+    //alias(libs.plugins.androidApplication)
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -34,7 +36,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    // Import the Firebase BoM
+    //noinspection UseTomlInstead
+    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
+    //noinspection UseTomlInstead
+    implementation("com.google.firebase:firebase-analytics")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
+
 }
