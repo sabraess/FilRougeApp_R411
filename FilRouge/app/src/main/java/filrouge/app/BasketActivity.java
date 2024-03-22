5e5435a67489ce771b26d143e06025f541f7f882
Essalah Sabra <sabra.essalah@etu.unice.fr>
2024-03-22 13:34:38 +0100

    Selected car Activity + basket activity

--- FilRouge/app/src/main/java/filrouge/app/BasketActivity.java
+++ FilRouge/app/src/main/java/filrouge/app/BasketActivity.java
@@ -1,6 +1,9 @@
 package filrouge.app;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class BasketActivity extends AppCompatActivity {
 
+    private final String TAG = "BasketActivity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basket);
+
+        ListView listView = findViewById(R.id.listViewPanier);
+
+
+
+        // Adapter adapter = new A(CarsList.getDisplayCars(), this);
+       // listView.setAdapter(carsAdapter);
+
 
     }
 }
