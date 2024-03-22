3cd92aad6827989d18cf0c2f00cf17e7d9a26ea1
Essalah Sabra <sabra.essalah@etu.unice.fr>
2024-03-22 14:34:47 +0100

    Selected car activity (vue modifiée et finie)

--- FilRouge/app/src/main/java/filrouge/app/SelectedCarActivity.java
+++ FilRouge/app/src/main/java/filrouge/app/SelectedCarActivity.java
@@ -8,6 +8,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 * author :
 * Modifié par :
@@ -15,16 +19,15 @@ import androidx.appcompat.app.AppCompatActivity;
 */
 public class SelectedCarActivity extends AppCompatActivity {
 
+    private final List<CarsList> displayedCars = new ArrayList<>(); //displayed list
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected_car);
 
+        int indexCar = getIntent().getIntExtra("cars", 0);
 
-        Intent intent = getIntent();
-        int index = intent.getExtras().getInt("cars");
-
-        System.out.println(index);
+        System.out.println(indexCar);
 
         TextView carSelectedName = findViewById(R.id.carSelectedName);
 
@@ -33,11 +36,35 @@ public class SelectedCarActivity extends AppCompatActivity {
         ImageView imageViewAnimation = findViewById(R.id.imageViewCarTurningAround);
 
         // Ajout de l'animation
-        imageViewAnimation.setBackgroundResource(R.drawable.car_turning_animation);
-        AnimationDrawable anim= (AnimationDrawable)imageViewAnimation.getBackground();
-        anim.start();
+        // imageViewAnimation.setBackgroundResource(R.drawable.car_turning_animation);
+        // AnimationDrawable anim= (AnimationDrawable)imageViewAnimation.getBackground();
+        // anim.start();
 
 
+        Car carSelected = new Car();
+/*
+        displayedCars.getInstance().get(index);
+
+        TextView name = findViewById(R.id.characterName);
+        name.setText(character.getName());
+        TextView description = findViewById(R.id.description);
+        description.setText(character.getDescription());
+        ImageView picture = findViewById(R.id.picture);
+        picture.setImageResource(character.getPicture());
+
+        Intent intent = getIntent();
+        Car carSelectedD = intent.getParcelableExtra(getString(R.string.CHARACTER_KEY));
+
+        Log.d(TAG, "character = " + currentCharacter);
+        TextView name = findViewById(R.id.characterName);
+        ToggleButton favorite = findViewById(R.id.characterFavorite);
+        ImageView picture = findViewById(R.id.characterPicture);
+        TextView description = findViewById(R.id.characterDescription);
+        name.setText(currentCharacter.getName());
+        favorite.setChecked( currentCharacter.isFavorite() );
+        picture.setImageResource(currentCharacter.getPicture());
+        description.setText(currentCharacter.getDescription());
+*/
         /*
         Intent intent = getIntent();
         int index = intent.getIntExtra("cars",0);
